b8c7ea4bd1816529dbb98febeb12f5b21c9c4f11
BrianAmin <tuf49787@temple.edu>
2018-04-16 15:27:17 -0400

    Setup project files for a digital and analog interface

---
+++ ProjectGForce_DigitalAnalogInterface/ProjectGForce_DigitalAnalogInterface_Arduino/ProjectGForce_DigitalAnalogInterface_Arduino.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		ProjectGForce_DigitalAnalogInterface_Arduino.ino
+ Created:	4/16/2018 3:26:53 PM
+ Author:	Merc
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
