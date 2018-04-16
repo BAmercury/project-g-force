a86582dc7c280eed1e27367e85ce3d7deaffcbbd
BrianAmin <tuf49787@temple.edu>
2018-04-16 13:53:46 -0400

    Seems like having the screen update through the loop was causing the rpm_stepper motor to jitter. Commented it out. Also setSpeed is RPM not steps/second so no conversion was necessary

--- ProjectGForce_AnalogInterface/ProjectGForce_AnalogInterface/ProjectGForce_AnalogInterface.ino
+++ ProjectGForce_AnalogInterface/ProjectGForce_AnalogInterface/ProjectGForce_AnalogInterface.ino
@@ -24,15 +24,16 @@ Adafruit_RGBLCDShield screen = Adafruit_RGBLCDShield();
 
 
 // user input control variable. Reset if device resets
-bool user_input = true;
+boolean user_input = true;
 
-// Analog input pins
+// Analog input pins values
 int rpm_stick = 0;
 int angle_stick = 0;
 
 // rotation and holding stuff
 const int steps_per_revolution = 200;
 
+float desired_speed;
 
 AccelStepper rpm_stepper(AccelStepper::FULL4WIRE, 8, 9, 10, 11);
 AccelStepper angle_stepper(AccelStepper::FULL4WIRE, 4, 5, 6, 7);
@@ -64,16 +65,17 @@ struct CommandPacket prompt_user_input()
 	while (user_veri)
 	{
 
-
-
+    screen.noBlink();
+    screen.clear();
 		screen.setCursor(0, 0);
-		screen.print("Desired RPM: ");
-		rpm_stick = analogRead(0);
-		rpm_desired = map(rpm_stick, 0, 1023, 0, 150);
+		screen.print("RPM: ");
+		//rpm_stick = analogRead(0);
+		//rpm_desired = map(rpm_stick, 0, 1023, 0, 150);
+		rpm_desired = 100;
 		screen.print(rpm_desired);
-		screen.blink();
+
 		screen.setCursor(0, 1);
-		screen.print("Desired Angle: ");
+		screen.print("Angle: ");
 		angle_stick = analogRead(1);
 		angle_desired = map(angle_stick, 0, 1023, 0, 90);
 		screen.print(angle_desired);
@@ -108,13 +110,12 @@ void setup() {
 	screen.setBacklight(WHITE);
 	Serial.begin(115200);
 
-	rpm_stepper.setAcceleration(0.5);
-	rpm_stepper.setMaxSpeed(500); //500 steps per second
+	//rpm_stepper.setAcceleration(100);
+	rpm_stepper.setMaxSpeed(1000); //500 steps per second
 
-	angle_stepper.setAcceleration(0.1);
-	angle_stepper.setMaxSpeed(33.33); // 10 RPM convereted to steps
+	angle_stepper.setAcceleration(50);
+	angle_stepper.setMaxSpeed(500); // 10 RPM convereted to steps
 	angle_stepper.setCurrentPosition(0);
-
 
 
 }
@@ -128,33 +129,44 @@ void loop() {
 		Serial.println("Got commands");
 		Serial.println(command_packet.angle_desired);
 		Serial.println(command_packet.rpm_desired);
+		// Convert Desired RPM to Steps/Second
+		//desired_speed = command_packet.rpm_desired * (3.3333);
+		//Serial.println("Desired Speed is still: ");
+		// Sets speed in Steps per Second
+		rpm_stepper.setSpeed(command_packet.rpm_desired);
 		user_input = false;
 
 	}
 
+	
+
 	if (user_input == false)
 	{
-		screen.clear();
-		screen.setBacklight(TEAL);
-		screen.setCursor(0, 0);
-		screen.print("RUNNING TEST");
+		//screen.clear();
+		//screen.setBacklight(RED);
+		//screen.setCursor(0, 0);
+		//screen.print("RUNNING TEST");
 		set_angle(command_packet.angle_desired);
-		set_rpm(command_packet.rpm_desired);
+		Serial.println("Speed of motor: ");
+		Serial.println(rpm_stepper.speed());
+		rpm_stepper.runSpeed();
+		//set_rpm(desired_speed);
 
 		// See if user has pushed the stop button. Set user_input back to true and wait for next set of commands
 		uint8_t buttons = screen.readButtons();
 		if (buttons & BUTTON_SELECT) {
 			screen.setBacklight(WHITE);
 			screen.clear();
-			screen.print("Stopping Test");
+			screen.print("STOPPING Test");
 			rpm_stepper.stop();
 			angle_stepper.runToNewPosition(0);
+		    angle_stepper.stop();
 			user_input = true;
 
 		}
 	}
 
-
+	
 	
   
 }
@@ -162,11 +174,15 @@ void loop() {
 void set_angle(int desired_angle)
 {
 	// Convert desired angle to steps
-	long position = desired_angle * (2.85714); // Angle to Steps based on step angle of 0.35
+	long position = desired_angle / (0.35); // Angle to Steps based on step angle of 0.35
+
+  //angle_stepper.setSpeed(100);
 	if (angle_stepper.currentPosition() != position)
 	{
 		// Blocks until it reaches its position, then the RPM code should kick in
-		angle_stepper.runToNewPosition(desired_angle);
+		angle_stepper.runToNewPosition(-position);
+    //Serial.println("Traveling to: ");
+    //Serial.println(position);
 
 
 	}
@@ -176,13 +192,9 @@ void set_angle(int desired_angle)
 
 }
 
-void set_rpm(int desired_rpm)
+void set_rpm(float desired_rpm)
 {
-	// Convert Desired RPM to Steps/Second
-	float desired_speed = desired_rpm * (3.3333);
-
-	// Sets speed in Steps per Second
-	rpm_stepper.setSpeed(desired_rpm);
+	Serial.println(desired_rpm);
 	rpm_stepper.runSpeed();
 
 
